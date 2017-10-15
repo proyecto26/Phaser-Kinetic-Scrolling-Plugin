bad0bef92c06c914a4ae6f0d9d093b61d2272d97
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2017-10-15 13:20:27 -0500

    Update version

--- dist/phaser-kinetic-scrolling-plugin.js
+++ dist/phaser-kinetic-scrolling-plugin.js
@@ -3,7 +3,7 @@
  * @author       Juan Nicholls <jdnichollsc@hotmail.com>
  * @copyright    2015 Juan Nicholls - http://jdnichollsc.github.io/Phaser-Kinetic-Scrolling-Plugin/
  * @license      {@link http://opensource.org/licenses/MIT}
- * @version 1.0.4
+ * @version 1.0.5
  */
 
 (function (Phaser) {
